9637eca905235703c3a113aa985fc99d08c48f3f
Big Bertha <none>
2011-06-28 18:26:41 +0100

    Added missing files from last commit

---
+++ keyPress.m
@@ -0,0 +1,27 @@
+function keyPress(src, eventInfo)
+
+global state;
+
+if isempty(eventInfo.Character)
+  return;
+end
+
+switch eventInfo.Key
+  case 'uparrow'
+    state.dataGain = state.dataGain*1.25;
+  case 'downarrow'
+    state.dataGain = state.dataGain/1.25;
+end
+
+switch eventInfo.Character
+  case 'p'
+    state.plot.enabled = ~state.plot.enabled;
+  case 'r'
+    state.plot.raster = ~state.plot.raster;
+  case 'w'
+    state.plot.waveform = ~state.plot.waveform;
+  case 'f'
+    state.plot.filtered = ~state.plot.filtered;
+  case 'l'
+    state.plot.lfp = ~state.plot.lfp;
+end
---
+++ scopeTraceFastReset.m
@@ -0,0 +1,18 @@
+function plotData = scopeTraceFastReset(plotData)
+
+nSamplesExpected = plotData.nSamplesExpected;
+fs_in = plotData.fs_in;
+plotData.plotIndex = zeros(1,32);
+
+if plotData.clean
+  return;
+end
+
+for chan = 1:32
+  ax = plotData.subplotHandles(chan);
+  cla(ax);
+  line([1 nSamplesExpected]/fs_in, [0 0], 'color', [0 0 0], 'parent', plotData.subplotHandles(chan),'hittest','off');
+  line([1 1]/fs_in, [-1 1], 'color', [0 0 0],'parent',plotData.subplotHandles(chan),'hittest','off');
+end
+
+plotData.clean = true;
