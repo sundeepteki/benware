359e75e61373b9ab03b59951d8a5c371ab2f2e99
Ben <ben@oxfordhearing.com>
2011-07-15 16:50:28 +0100

    Add sandra pe grids

---
+++ grids/grid_pe_73dB.m
@@ -0,0 +1,21 @@
+function grid = grid_pe()
+
+  % controlling the sound presentation
+  grid.stimGenerationFunctionName = 'loadStereo';
+  grid.stimDir = 'E:\auditory-objects\sounds.calib.expt%E\pe\';
+  grid.sampleRate = 24414.0625*2;  % ~50kHz
+
+  % essentials
+  grid.name = 'pe.83dB';
+  grid.stimFilename = 'pe.id.%1.token.%2.%L.f32';
+  
+  % stimulus grid structure
+  grid.stimGridTitles = {'ID', 'Token', 'Level'};
+  grid.stimGrid = combvec(408:436, 1:50, 73)';
+  
+  % sweep parameters
+  grid.sweepLength = .42;
+  grid.repeatsPerCondition = 1;
+  
+  % set this using absolute calibration
+  grid.stimLevelOffsetDB = -84;
