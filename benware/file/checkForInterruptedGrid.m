200b2426f30c18ceaeb00159e03a81581b285fa0
Von Braun <vonbraun@>
2011-07-18 09:21:50 +0100

    Stop checkForInterruptedGrid from crashing if gridInfo.mat is missing

--- benware/file/checkForInterruptedGrid.m
+++ benware/file/checkForInterruptedGrid.m
@@ -3,23 +3,27 @@ function [gridFile, lastSweep] = checkForInterruptedGrid(exptDir, expt)
 % in exptDir represents an unfinished grid. if so, return it and the 
 % number of sweeps that were successfully run
 
+gridFile = [];
+lastSweep = 0;
+
 % find the most recently created directory in the expt dir
 newestDir = findNewestSubdir(exptDir);
 
 if isempty(newestDir)
-  gridFile = [];
-  lastSweep = 0;
   return;
 end
 
 % load the grid from this directory
-gridInfo = load([exptDir newestDir.name filesep 'gridInfo.mat']);
+gridFilename = [exptDir newestDir.name filesep 'gridInfo.mat'];
+if ~exist(gridFilename, 'file')
+  return;
+end
+
+gridInfo = load(gridFilename);
 
 filename = constructDataPath([gridInfo.expt.dataDir gridInfo.expt.sweepFilename], gridInfo.grid, gridInfo.expt, gridInfo.grid.nSweepsDesired);
 if exist(filename, 'file')
   % then the last grid is complete
-  gridFile = [];
-  lastSweep = 0;
   return;
 else
   for ii = 1:gridInfo.grid.nSweepsDesired
