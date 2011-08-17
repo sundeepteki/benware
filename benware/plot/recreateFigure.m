9ce76c7ce3580e964a212f9b4ecd4dedd2409b75
Ben Willmore <bdeb@willmore.eu>
2011-08-17 12:02:34 +0100

    Function for recreating figure window
    Intended to help with long pauses after long grids, and the
    error 'IUJ somethingsomething waiting for figure to show up.

---
+++ benware/plot/recreateFigure.m
@@ -0,0 +1,38 @@
+function recreateFigure(figNum)
+
+if ~exist('figNum', 'var')
+  figNum = gcf;
+end
+
+% move children (subplots) to another figure
+children = get(figNum, 'children');
+figure(110876);
+for child = children
+  set(child, 'parent', 110876);
+end
+
+% store various properties of the figure
+properties = {'color', 'renderer', 'KeyPressFcn', 'name', 'numbertitle', ...
+              'toolbar', 'menubar', 'position'};
+values = [];
+for ii = 1:length(properties)
+  values{ii} = get(figNum, properties{ii});
+end
+
+% close figure and open a new one
+close(figNum);
+figure(figNum);
+
+% restore the subplots to the new figure
+children = get(110876, 'children');
+for child = children
+  set(child, 'parent', figNum);
+end
+
+% restore the other properties
+for ii = 1:length(properties)
+  set(figNum, properties{ii}, values{ii});
+end
+
+% close the temporary figure
+close(110876);
