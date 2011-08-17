c6e06dfb92adc43a1aa442ab0bb68f673604b4ef
Ben Willmore <bdeb@willmore.eu>
2011-08-17 12:23:01 +0100

    Less intrusive visual bell; TDT window invisible

--- benware/ui/bbeep.m
+++ benware/ui/bbeep.m
@@ -6,12 +6,13 @@ global state
 
 if isfield(state, 'visualBell') && state.visualBell
 
-  figure(999);
-  set(999,'menubar','none','toolbar','none');
-  a = subplot(1,1,1);
-  set(a, 'position', [0 0 1 1], 'color', [1 0 0], 'xtick', [], 'ytick', []);
-  makeFigFullscreen;
-  set(gcf, 'position', get(gcf, 'position') + [8 46 -16 -54]);
+  %figure(999);
+  %set(999,'menubar','none','toolbar','none');
+  %a = subplot(1,1,1);
+  %set(a, 'position', [0 0 1 1], 'color', [1 0 0], 'xtick', [], 'ytick', []);
+  %makeFigFullscreen;
+  %set(gcf, 'position', get(gcf, 'position') + [8 46 -16 -54]);
+  set(101, 'color', [1 0 0]);
 else
 
  fs= 44100;
