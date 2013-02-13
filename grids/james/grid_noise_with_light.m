a96f2ed1b0e877ff80b163b4c17b63871230bf67
Ben Willmore <bdeb@willmore.eu>
2013-02-13 15:28:21 +0000

    changes on rig machine

--- grids/james/grid_noise_with_light.m
+++ grids/james/grid_noise_with_light.m
@@ -10,8 +10,15 @@ function grid = grid_monosearchnoise()
   % stimulus grid structure
   grid.stimGridTitles = {'Stimulus Length (ms)', 'Noise Delay (ms)', ...
       'Noise Length (ms)', 'Light voltage (V)', 'Light delay (ms)', 'Light Duration (ms)', 'Level'};
-  grid.stimGrid = [1000 25 50 5 0.01 75 80; 1000 25 50 0 0.01 75 80;];
+  grid.stimGrid = [1000 300 50 5 0.01 700 80; 1000 300 50 0 0.01 700 80;];
 
+  % for jonathan
+  %fprintf('**jonathans mouse!!**');
+  %grid.stimGrid = [1000 25 5 0 25 5 80; ...
+  %                 1000 25 5 5 25 5 -80; ...
+  %                 1000 25 5 5 25 5 80; ...
+  %                ];
+  
   % sweep parameters
   grid.postStimSilence = 0;
   grid.repeatsPerCondition = 200;
