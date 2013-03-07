be3e891d5dcc24e74adda21096bc271e0d59ca5b
Ben Willmore <bdeb@willmore.eu>
2013-03-07 21:32:06 +0000

    whitespace changes

--- benware/hardware/tdtDataDevice.m
+++ benware/hardware/tdtDataDevice.m
@@ -1,6 +1,6 @@
 classdef tdtDataDevice < tdtDevice
   properties
-      nChannels = nan;
+    nChannels = nan;
   end
 
   methods
@@ -24,67 +24,67 @@ classdef tdtDataDevice < tdtDevice
     end
     
     function data = downloadAllData(obj)
-        data = cell(1, obj.nChannels);
-        for chan = 1:obj.nChannels
-            maxIndex = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
-            data{chan} = obj.handle.ReadTagV(['ADwb' num2str(chan)],0,maxIndex);
-        end
+      data = cell(1, obj.nChannels);
+      for chan = 1:obj.nChannels
+          maxIndex = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
+          data{chan} = obj.handle.ReadTagV(['ADwb' num2str(chan)],0,maxIndex);
+      end
     end
 
     function data = downloadData(obj, chan, offset)
-        maxIndex = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
-        if maxIndex-offset==0
-          data = [];
-        elseif maxIndex<offset
-          data = [];
-          errorBeep('Data requested beyond end of buffer!\n');
-        else
-          data = obj.handle.ReadTagV(['ADwb' num2str(chan)],offset,maxIndex-offset);
-        end
+      maxIndex = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
+      if maxIndex-offset==0
+        data = [];
+      elseif maxIndex<offset
+        data = [];
+        errorBeep('Data requested beyond end of buffer!\n');
+      else
+        data = obj.handle.ReadTagV(['ADwb' num2str(chan)],offset,maxIndex-offset);
+      end
     end
     
     function reset(obj, trialLen)
-        if nargin==2
-          obj.handle.SetTagVal('recdur',trialLen);
-        end
-        obj.handle.SoftTrg(9);
+      if nargin==2
+        obj.handle.SetTagVal('recdur',trialLen);
+      end
+      obj.handle.SoftTrg(9);
     end
     
     function setAudioMonitorChannel(obj, chan)
-        obj.handle.SetTagVal('MonChan',chan);
+      obj.handle.SetTagVal('MonChan',chan);
     end
     
     function softTrigger(obj)
-        obj.handle.SoftTrg(1);
+      obj.handle.SoftTrg(1);
     end
 
     function index = countAllData(obj, nChannels)
-        % index = countAllData(dataDevice, nChannels)
-        %
-        % Count the number of samples available on each channel of the data Device
-        % i.e. the index that the serial buffers have reached
-        % 
-        % dataDevice: A handle to the data device
-        % nChannels: The number of channels that you want information about
-        % index: 1xnChannels vector of buffer indexes
+      % index = countAllData(dataDevice, nChannels)
+      %
+      % Count the number of samples available on each channel of the data Device
+      % i.e. the index that the serial buffers have reached
+      % 
+      % dataDevice: A handle to the data device
+      % nChannels: The number of channels that you want information about
+      % index: 1xnChannels vector of buffer indexes
 
-        index = nan(1, nChannels);
-        for chan = 1:nChannels
-            index(chan) = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
-        end
+      index = nan(1, nChannels);
+      for chan = 1:nChannels
+          index(chan) = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
+      end
     end
     
     function index = countData(obj, chan)
-        % index = countData(dataDevice, chan)
-        %
-        % Count number of samples available on a specified channel of the 
-        % data device
-        %
-        % dataDevice: handle of the data device
-        % chan: the number of the channel you want
-        % index: the index that the serial buffer has reached
+      % index = countData(dataDevice, chan)
+      %
+      % Count number of samples available on a specified channel of the 
+      % data device
+      %
+      % dataDevice: handle of the data device
+      % chan: the number of the channel you want
+      % index: the index that the serial buffer has reached
 
-        index = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
+      index = obj.handle.GetTagVal(['ADidx' num2str(chan)]);
     end
     
   end
--- benware/hardware/tdtStimDevice.m
+++ benware/hardware/tdtStimDevice.m
@@ -1,156 +1,156 @@
 classdef tdtStimDevice < tdtDevice
-  properties
+	properties
+	end
+
+	methods
+
+	function obj = tdtStimDevice(deviceName, requestedSampleRateHz, nChannels)
+
+		if nChannels==1
+			rcxFilename = 'benware/tdt/monoplay.rcx';
+			versionTagName = 'MonoPlayVer';
+			versionTagValue = 3;
+		elseif nChannels==2
+			rcxFilename = 'benware/tdt/stereoplay.rcx';
+			versionTagName = 'StereoPlayVer';
+			versionTagValue = 5;
+		else
+			errorBeep('Can only do mono or stereo\n');
+		end
+		
+		obj = obj@tdtDevice(deviceName, rcxFilename, versionTagName, versionTagValue, ...
+			requestedSampleRateHz);
+
+		if strcmp(deviceName, 'RX8')
+			channel.L = 20;
+			channel.R = 18;
+
+		elseif strcmp(deviceName, 'RX6')
+			channel.L = 1;
+			channel.R = 2;
+			
+		else
+			errorBeep('I don''t know the output channel numbers for this device\n');
+		end
+
+		obj.handle.SetTagVal('LeftChannel', channel.L);
+		obj.handle.SetTagVal('RightChannel', channel.R);
+
+	end
+
+	function stim = downloadStim(obj, offset, nSamples, nStimChans)
+    % why is nStimChans specified here? It should be obvious 
+    % from the stimDevice. i think that however channel numbers
+    % are specified should be rationalised
+    stim(1,:) = obj.handle.ReadTagV('WaveformL', offset, nSamples);
+        
+    if nStimChans==2
+    	stim(2,:) = obj.handle.ReadTagV('WaveformR', offset, nSamples);
+    end
   end
 
-  methods
+	  function index = getStimIndex(obj)
+	  	index = obj.handle.GetTagVal('StimIndex');
+	  end
+	  
+	  function nSamples = getStimLength(obj)
+	  	nSamples = obj.handle.GetTagVal('nSamples');
+	  end
+	  
+	  function reset(obj)
+	  	obj.handle.SoftTrg(9);
+	  end
+	  
+	  function setActiveStimChannels(obj, nChannels)
+	  	obj.handle.SetTagVal('LeftActive', 1);
 
-    function obj = tdtStimDevice(deviceName, requestedSampleRateHz, nChannels)
+	  	if nChannels==1
+	  		obj.handle.SetTagVal('RightActive', 0);
+	  	else
+	  		obj.handle.SetTagVal('RightActive', 1);    
+	  	end
+	  end
+	  
+	  function setStimLength(obj, samples)
+	  	if ~obj.handle.SetTagVal('nSamples', samples)
+	  		errorBeep('WriteTag nSamples failed');
+	  	end
+	  end
+	  
+	  function softTriggerStimulus(stimDevice)
+	  	invoke(stimDevice,'SoftTrg',1);
+	  end
+	  
+	  function uploadStim(obj, stim, offset)
+	    % Note it's also necessary to set nSamples for the new stimulus
 
-      if nChannels==1
-          rcxFilename = 'benware/tdt/monoplay.rcx';
-          versionTagName = 'MonoPlayVer';
-          versionTagValue = 3;
-      elseif nChannels==2
-          rcxFilename = 'benware/tdt/stereoplay.rcx';
-          versionTagName = 'StereoPlayVer';
-          versionTagValue = 5;
-      else
-          errorBeep('Can only do mono or stereo\n');
-      end
+	    %[offset offset+size(stim,2) getStimIndex]
+	    %if ~stimDevice.WriteTagV('WaveformL',offset,stim(1,:))
+	    %    errorBeep('WriteTagV WaveformL failed');
+	    %end
+	    %if ~stimDevice.WriteTagV('WaveformR',offset,stim(2,:))
+	    %    errorBeep('WriteTagV WaveformR failed');
+	    %end;
+
+	    maxRetries = 2;
+
+	    nRetries = 0;
+	    success = false;
+
+	    while ~success && nRetries<maxRetries
+	    	success = obj.handle.WriteTagV('WaveformL',offset,stim(1,:));
+	    	if ~success
+	    		fprintf('== WriteTagV WaveformL failed\n');
+	    		nRetries = nRetries + 1;
+	    	end
+	    end
+
+	    if ~success
+	    	errorBeep('Giving up after 3 attempts!');
+	    end
+
+	    if size(stim,1)==1
+	    	return
+	    end
+
+	    nRetries = 0;
+	    success = false;
+
+	    while ~success && nRetries<maxRetries
+	    	success = obj.handle.WriteTagV('WaveformR',offset,stim(2,:));
+	    	if ~success
+	    		fprintf('== WriteTagV WaveformR failed\n');
+	    		nRetries = nRetries + 1;
+	    	end
+	    end
+
+	    if ~success
+	    	errorBeep('Giving up after 3 attempts!');
+	    end
+	  end
+
+	  function uploadWholeStim(obj, stim)
+	    % uploadWholeStimulus(obj, stim)
+	    %
+	    % Upload a stereo stimulus to stimDevice, and inform the device
+	    % about the stimulus length
+
+	    if ~obj.handle.SetTagVal('nSamples',size(stim,2))
+	    	errorBeep('WriteTag nSamples failed');
+	    end
+
+	    if ~obj.handle.WriteTagV('WaveformL',0,stim(1,:))
+	    	errorBeep('WriteTagV WaveformL failed');
+	    end
+
+	    if size(stim, 1)==1
+	    	return
+	    end
+
+	    if ~obj.handle.WriteTagV('WaveformR',0,stim(2,:))
+	    	errorBeep('WriteTagV WaveformR failed');
+	    end
+	  end
       
-      obj = obj@tdtDevice(deviceName, rcxFilename, versionTagName, versionTagValue, ...
-                   requestedSampleRateHz);
-
-      if strcmp(deviceName, 'RX8')
-          channel.L = 20;
-          channel.R = 18;
-
-      elseif strcmp(deviceName, 'RX6')
-          channel.L = 1;
-          channel.R = 2;
-      
-      else
-          errorBeep('I don''t know the output channel numbers for this device\n');
-      end
-
-      obj.handle.SetTagVal('LeftChannel', channel.L);
-      obj.handle.SetTagVal('RightChannel', channel.R);
-
-    end
-
-    function stim = downloadStim(obj, offset, nSamples, nStimChans)
-        % why is nStimChans specified here? It should be obvious 
-        % from the stimDevice. i think that however channel numbers
-        % are specified should be rationalised
-        stim(1,:) = obj.handle.ReadTagV('WaveformL', offset, nSamples);
-        
-        if nStimChans==2
-          stim(2,:) = obj.handle.ReadTagV('WaveformR', offset, nSamples);
-        end
-    end
-
-    function index = getStimIndex(obj)
-        index = obj.handle.GetTagVal('StimIndex');
-    end
-    
-    function nSamples = getStimLength(obj)
-        nSamples = obj.handle.GetTagVal('nSamples');
-    end
-    
-    function reset(obj)
-        obj.handle.SoftTrg(9);
-    end
-    
-    function setActiveStimChannels(obj, nChannels)
-        obj.handle.SetTagVal('LeftActive', 1);
-
-        if nChannels==1
-          obj.handle.SetTagVal('RightActive', 0);
-        else
-          obj.handle.SetTagVal('RightActive', 1);    
-        end
-    end
-    
-    function setStimLength(obj, samples)
-        if ~obj.handle.SetTagVal('nSamples', samples)
-            errorBeep('WriteTag nSamples failed');
-        end
-    end
-    
-    function softTriggerStimulus(stimDevice)
-        invoke(stimDevice,'SoftTrg',1);
-    end
-    
-    function uploadStim(obj, stim, offset)
-        % Note it's also necessary to set nSamples for the new stimulus
-
-        %[offset offset+size(stim,2) getStimIndex]
-        %if ~stimDevice.WriteTagV('WaveformL',offset,stim(1,:))
-        %    errorBeep('WriteTagV WaveformL failed');
-        %end
-        %if ~stimDevice.WriteTagV('WaveformR',offset,stim(2,:))
-        %    errorBeep('WriteTagV WaveformR failed');
-        %end;
-
-        maxRetries = 2;
-
-        nRetries = 0;
-        success = false;
-
-        while ~success && nRetries<maxRetries
-          success = obj.handle.WriteTagV('WaveformL',offset,stim(1,:));
-          if ~success
-            fprintf('== WriteTagV WaveformL failed\n');
-            nRetries = nRetries + 1;
-          end
-        end
-
-        if ~success
-          errorBeep('Giving up after 3 attempts!');
-        end
-
-        if size(stim,1)==1
-          return
-        end
-
-        nRetries = 0;
-        success = false;
-
-        while ~success && nRetries<maxRetries
-          success = obj.handle.WriteTagV('WaveformR',offset,stim(2,:));
-          if ~success
-            fprintf('== WriteTagV WaveformR failed\n');
-            nRetries = nRetries + 1;
-          end
-        end
-
-        if ~success
-          errorBeep('Giving up after 3 attempts!');
-        end
-    end
-
-    function uploadWholeStim(obj, stim)
-        % uploadWholeStimulus(obj, stim)
-        %
-        % Upload a stereo stimulus to stimDevice, and inform the device
-        % about the stimulus length
-
-        if ~obj.handle.SetTagVal('nSamples',size(stim,2))
-            errorBeep('WriteTag nSamples failed');
-        end
-
-        if ~obj.handle.WriteTagV('WaveformL',0,stim(1,:))
-            errorBeep('WriteTagV WaveformL failed');
-        end
-
-        if size(stim, 1)==1
-          return
-        end
-
-        if ~obj.handle.WriteTagV('WaveformR',0,stim(2,:))
-            errorBeep('WriteTagV WaveformR failed');
-        end
-    end
-    
   end
 end
