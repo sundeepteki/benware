a136222c33b6930cc8bd0bee99c811f32487cfce
Ben Willmore <bdeb@willmore.eu>
2013-03-07 14:26:39 +0000

    update to previous

--- benware/hardware/tdtStimDevice.m
+++ benware/hardware/tdtStimDevice.m
@@ -1,16 +1,43 @@
 classdef tdtStimDevice < handle
   properties
-   stimDevice = nan;
-   dataDevice = nan;
-   zBus = nan;
+   deviceName = '';
+   deviceHandle = [];
+   sampleRate = -1;
+   nChannels = -1;
   end
 
   methods
 
-    function obj = tdtRig(stimDeviceInfo, dataDeviceInfo)
-      obj.stimDevice = tdtStimDevice();
-      %obj.dataDevice = tdtDataDevice();
-      %obj.zBus = zBus();
+    function obj = tdtStimDevice(deviceInfo)
+      obj.deviceName = deviceInfo.deviceName;
+      obj.nChannels = deviceInfo.nChannels;
+      obj.deviceHandle = tdtStimDevice();
+
+      if nChannels==1
+	    [obj.deviceHandle, obj.sampleRate] = deviceInit(device, deviceName, ...
+     	  'benware/tdt/monoplay.rcx', 'MonoPlayVer', 3, requestedSampleRate);
+  else
+    [device, sampleRate] = deviceInit(device, deviceName, ...
+      'benware/tdt/stereoplay.rcx', 'StereoPlayVer', 5, requestedSampleRate);
+  end
+  
+  if strcmp(deviceName, 'RX8')
+      channel.L = 20;
+      channel.R = 18;
+      
+  elseif strcmp(deviceName, 'RX6')
+      channel.L = 1;
+      channel.R = 2;
+      
+  else
+      errorBeep('I don''t know the output channel numbers for this device\n');
+  end
+  
+  device.SetTagVal('LeftChannel', channel.L)
+  device.SetTagVal('RightChannel', channel.R)
+
+
+
     end
 
   end
