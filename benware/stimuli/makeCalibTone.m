b981c4e1d3bba0880888d7e899bc6ec59daf7d61
Ben Willmore <bdeb@willmore.eu>
2013-03-11 15:53:29 +0000

    more stimulus interface stuff

--- benware/stimuli/makeCalibTone.m
+++ benware/stimuli/makeCalibTone.m
@@ -1,13 +1,5 @@
-function [stim, stimInfo] = makeCalibTone(sweepNum, grid, expt)
-
-stimInfo.stimGridTitles = grid.stimGridTitles;
-stimInfo.stimParameters = grid.randomisedGrid(sweepNum, :);
-
-parameters = grid.randomisedGrid(sweepNum,1:end);
-
-freq = parameters(1);
-duration = parameters(2);
-level = parameters(3);
+function stim = makeCalibTone(expt, grid, sampleRate, nChannels, compensationFilters, ...
+								freq, duration, level)
 
 % time
 t = 0:1/grid.sampleRate:duration/1000;
@@ -21,13 +13,24 @@ ramp = (1-cos(pi*(1:ramplen_samples)/ramplen_samples))/2;
 env = [ramp ones(1,length(uncalib)-2*length(ramp)) fliplr(ramp)];
 uncalib = uncalib.*env;
 
-% convolve with compensation filter
-%keyboard
-for chan = 1:length(grid.compensationFilters)
-	stim(chan, :) = conv(uncalib, grid.compensationFilters{chan});
+% % convolve with compensation filters
+% for chan = 1:length(grid.compensationFilters)
+%   stim(chan, :) = conv(uncalib, grid.compensationFilters{chan});
+% end
+% for chan = length(compensationFilters)+1:nChannels
+%   stim(chan, :) = 0;
+% end
+
+
+for chan = 1:length(compensationFilters)
+  cf = compensationFilters{chan};
+  ft = abs(fft(cf));
+  ft = ft(1:length(ft)/2);
+  f = linspace(0, sampleRate/2, length(ft));
+  amp(chan) = interp1(f, ft, freq);
 end
+fprintf('Untested compensation code in makeCalibTone!\n');
 
 % apply level offset
-for chan = 1:length(grid.compensationFilters)
-	stim(chan, :) = stim(chan, :) * 10^((level+grid.stimLevelOffsetDB(chan)) / 20);
-end
+level_offset = level-80;
+stim = stim * (10^(level_offset) / 20);
