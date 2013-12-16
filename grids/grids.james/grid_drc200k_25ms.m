772d730398953a0fb1975a2138165de611fe9df9
Ben Willmore <bdeb@willmore.eu>
2013-12-16 22:17:34 +0000

    loadMonoFileWithLight

--- grids/grids.james/grid_drc200k_25ms.m
+++ grids/grids.james/grid_drc200k_25ms.m
@@ -1,34 +1,30 @@
 function grid = grid_drc200k_25ms()
 
   % controlling the sound presentation
-  grid.sampleRate = 24414.0625*8;  % ~200kHz
-  grid.stimGenerationFunctionName = 'loadStimAndCompensateWithLight';
+  grid.sampleRate = tdt200k;
+  grid.stimGenerationFunctionName = 'loadMonoFileWithLight';
   grid.stimDir = 'e:\auditory-objects\sounds-uncalib\mouse.drc.8\';
-  %grid.stimDir = '/Users/ben/scratch/texture/texture.v2/renamed/';
   grid.stimFilename = 'drc%1_dur%3_contrast%2.f32';
   
   % stimulus grid structure
-  grid.stimGridTitles = {'DRC ID', 'Contrast', 'Chord dur', 'Light voltage', 'Level'};
+  grid.stimGridTitles = {'DRC ID', 'Contrast', 'Chord dur', 'Light voltage'};
   
   voltage = 5; % voltage to use for light
-  grid.stimGrid = createPermutationGrid(1:4, [10 20 30 40 99], 25, [0 voltage], 80);
+  grid.stimGrid = createPermutationGrid(1:4, [10 20 30 40 99], 25, [0 voltage]);
 
-  % for calibration
-%   grid.stimGrid = createPermutationGrid(9, 9, 80);
-%   fprintf('For calibration only!\n');
-%   pause;
-%   grid.stimGrid = createPermutationGrid(1, [10], 25, 0, 80); 
-
-% compensation filter
-  grid.initFunction = 'loadCompensationFilters';
-  grid.compensationFilterFile = ...
-    'e:\auditory-objects\calibration\expt32-11.11.13\calibL_200k.mat';
-  grid.compensationFilterVarNames = {'calibL_200k.filter'};
+  global CALIBRATE
+  if CALIBRATE
+    % for calibration
+    %grid.stimGrid = createPermutationGrid(9, 9);
+    fprintf('For calibration only!\n');
+    pause;
+    grid.stimGrid = createPermutationGrid(1, [10], 25, 0); 
+  end
 
   % sweep parameters
   grid.postStimSilence = 0;
   grid.repeatsPerCondition = 3;
   
-  % set this using absolute calibration
-  grid.stimLevelOffsetDB = [15 0];
+  % set this to match nominal level of 80dB
+  grid.legacyLevelOffsetDB = 0;
   
