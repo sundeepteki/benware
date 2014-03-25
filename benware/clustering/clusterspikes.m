29baed75da2ec50e47f116d58a641bc7b0018b00
Ben Willmore <bdeb@willmore.eu>
2014-03-25 17:17:04 +0000

    clear LD_LIBRARY_PATH

--- benware/clustering/clusterspikes.m
+++ benware/clustering/clusterspikes.m
@@ -30,17 +30,18 @@ for dirIdx = 1:length(dirs)
   dir = dirs{dirIdx};
   [paramsFile, nShanks] = benware2spikedetekt(dirs{dirIdx});
   cmd = ['cd ' dir filesep 'spikedetekt' '; ' ...
-         'DYLD_LIBRARY_PATH='''' DYLD_FRAMEWORK_PATH='''' python ' pwd '/klustakwik/detektspikes.py ' paramsFile];
-  fprintf('= Detecting spikes by running command:\n %s', cmd);
+         'LD_LIBRARY_PATH='''' DYLD_LIBRARY_PATH='''' DYLD_FRAMEWORK_PATH='''' python ' pwd '/klustakwik/detektspikes.py ' paramsFile];
+  fprintf('= Detecting spikes by running command:\n %s\n', cmd);
   system(cmd);
+keyboard
   spikedetektDirs = getdirsmatching([dir filesep 'spikedetekt_*/']);
   spikedetektDir = spikedetektDirs{end};
   
   for shankIdx = 1:nShanks
     parameters = '-UseDistributional 1 -MaxPossibleClusters 500 -MaskStarts 300 -PenaltyK 1 -PenaltyKLogN 0 -DropLastNFeatures 1';
     cmd = sprintf(['cd ' spikedetektDir '; ' ...
-                   'DYLD_LIBRARY_PATH='''' DYLD_FRAMEWORK_PATH='''' ' pwd '/klustakwik/KlustaKwik.' computer ' ' paramsFile(1:end-7) ' %d ' parameters], shankIdx);
-    fprintf('Clustering shank %d by running command:\n %s', shankIdx, cmd);
+                   'LD_LIBRARY_PATH='''' DYLD_LIBRARY_PATH='''' DYLD_FRAMEWORK_PATH='''' ' pwd '/klustakwik/KlustaKwik.' computer ' ' paramsFile(1:end-7) ' %d ' parameters], shankIdx);
+    fprintf('Clustering shank %d by running command:\n %s\n', shankIdx, cmd);
     system(cmd);
   end
 end
