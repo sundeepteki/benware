793fa28cd1c0fb352f8f9614a486efef9bcd15a4
Ben Willmore <bdeb@willmore.eu>
2014-03-31 21:52:58 +0100

    fix getfiles|dirsmatching

--- benware/file/getdirsmatching.m
+++ benware/file/getdirsmatching.m
@@ -4,7 +4,12 @@ if ispc
     data = getfilesmatching(searchstring);
 else
   list = ls('-Cd', searchstring);
-  files = strsplit(list,'\n');
+  lines = strsplit(list,'\n');
+  files = {};
+  for ii = 1:length(lines)
+    f = strsplit(lines{ii}, '\t');
+    files(end+1:end+length(f)) = f;
+  end
   if length(files{end})==0
     files = files(1:end-1);
   end
--- benware/file/getfilesmatching.m
+++ benware/file/getfilesmatching.m
@@ -8,7 +8,12 @@ if ispc
     data = cellfun(@(x) strrep(x,'\','/'), data, 'uni', false); % convert ugly "\" back to much less troublesome "/" filesep
 else
     list = ls('-C', searchstring);
-    files = strsplit(list,'\n');
+    lines = strsplit(list,'\n');
+    files = {};
+    for ii = 1:length(lines)
+      f = strsplit(lines{ii}, '\t');
+      files(end+1:end+length(f)) = f;
+    end
     if length(files{end})==0
       files = files(1:end-1);
     end
