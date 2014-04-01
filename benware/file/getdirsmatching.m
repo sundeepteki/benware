f81b0929d227b931f958038c9072cd379b26a2d8
Ben Willmore <bdeb@willmore.eu>
2014-04-01 21:09:21 +0100

    fix getfiles/dirsmatching

--- benware/file/getdirsmatching.m
+++ benware/file/getdirsmatching.m
@@ -1,17 +1,3 @@
 function data = getdirsmatching(searchstring)
 
-if ispc
-    data = getfilesmatching(searchstring);
-else
-  list = ls('-Cd', searchstring);
-  lines = strsplit(list,'\n');
-  files = {};
-  for ii = 1:length(lines)
-    f = strsplit(lines{ii}, '\t');
-    files(end+1:end+length(f)) = f;
-  end
-  if length(files{end})==0
-    files = files(1:end-1);
-  end
-  data = sort(files)';
-end
+data = getfilesmatching(searchstring, 'isdir==true');
