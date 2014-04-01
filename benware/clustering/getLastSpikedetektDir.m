ef7c1b2a2477401ba52bf37ef15e41bb0f322719
Ben Willmore <bdeb@willmore.eu>
2014-04-01 21:50:33 +0100

    fixes

--- benware/clustering/getLastSpikedetektDir.m
+++ benware/clustering/getLastSpikedetektDir.m
@@ -4,12 +4,18 @@ try
   spikedetektDirs = getdirsmatching([dir filesep 'spikedetekt_*']);
 catch
   spikedetektDir = '';
+  return;
 end
 
 % get rid of spurious null
 spikedetektDirs = spikedetektDirs(cellfun(@(x) ~isempty(x), spikedetektDirs));
 
 % sort to get the last one
+if isempty(spikedetektDirs)
+  spikedetektDir = '';
+  return;
+end
+  
 res = cellfun(@(x) regexp(x, 'spikedetekt_([0-9]+)', 'tokens'), spikedetektDirs, 'uni', false);
 n = cellfun(@(x) eval(x{1}{1}), res)';
 [srt, idx] = sort(n);
