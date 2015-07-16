8cd54bf0b544f84f789ee1e476b63fef70704710
Ben Willmore <bdeb@willmore.eu>
2015-07-16 21:53:15 +0100

    fix klusta params

--- benware/clustering/makeklustaparams.m
+++ benware/clustering/makeklustaparams.m
@@ -12,6 +12,42 @@ end
 
 gridName = grid.name;
 
+%%%%%
+fprintf('Making probe adjacency map...');
+probes = expt.probes;
+layout = {};
+type = {};
+
+for ii = 1:length(probes)
+  if strcmp(probes(ii).layout, 'Warp-16')
+    layout{ii} = [16 1];
+
+  elseif probes(ii).layout(1)=='A'
+    res = regexp(probes(ii).layout, 'A([0-9]+)x([0-9]+)', 'tokens');
+    nShanks = eval(res{1}{1});
+    nSites = eval(res{1}{2});
+    layout{ii} = [nShanks nSites];
+    if strfind(probes(1).layout, 'Buzs') || strfind(probes(1).layout, 'Busz')
+      type{ii} = 'buzsaki';
+    else
+      type{ii} = 'linear';
+    end
+  else
+    error('unknown probe layout -- talk to ben');
+  end
+end
+
+probeFile = constructDataPath([exptdir filesep gridName '.probe'], grid, expt, 1);
+makeadjacencygraph2(layout, type, probeFile);
+
+nSitesPerShank = [];
+for ii = 1:length(layout)
+  nSitesPerShank = [nSitesPerShank repmat(layout{ii}(2), [1 layout{ii}(1)])];
+end
+
+fprintf('done\n');
+
+%%%%%
 fprintf('Making spikedetekt parameter file...');
 
 % get current directory
@@ -47,9 +83,12 @@ params = regexprep(params, '%PROBE_FILE%', [gridName '.probe']);
 params = regexprep(params, '%SAMPLE_RATE%', sprintf('%0.6f', expt.dataDeviceSampleRate));
 params = regexprep(params, '%N_CHANNELS%', sprintf('%d', expt.nChannels));
 
-params = regexprep(params, '%MIN_CLUSTERS%', sprintf('%d', expt.nChannels/2));
-params = regexprep(params, '%MAX_CLUSTERS%', sprintf('%d', expt.nChannels*2));
-params = regexprep(params, '%MAX_POSS_CLUSTERS%', sprintf('%d', min(expt.nChannels*4, 500)));
+max_sites = min(8,max(nSitesPerShank));
+max_poss = min(max_sites*8, 256);
+params = regexprep(params, '%MASK_STARTS%', sprintf('%d', max_poss/2));
+params = regexprep(params, '%MIN_CLUSTERS%', sprintf('%d', max_sites/2));
+params = regexprep(params, '%MAX_CLUSTERS%', sprintf('%d', max_sites*2));
+params = regexprep(params, '%MAX_POSS_CLUSTERS%', sprintf('%d', max_poss));
 
 paramsFile = constructDataPath([exptdir filesep gridName '.params'], grid, expt, 1);
 fid = fopen(paramsFile, 'w');
@@ -57,36 +96,3 @@ fprintf(fid, params);
 fclose(fid);
 fprintf('done\n');
 
-fprintf('Making probe adjacency map...');
-probes = expt.probes;
-layout = {};
-type = {};
-
-for ii = 1:length(probes)
-  if strcmp(probes(ii).layout, 'Warp-16')
-    layout{ii} = [16 1];
-
-  elseif probes(ii).layout(1)=='A'
-    res = regexp(probes(ii).layout, 'A([0-9]+)x([0-9]+)', 'tokens');
-    nShanks = eval(res{1}{1});
-    nSites = eval(res{1}{2});
-    layout{ii} = [nShanks nSites];
-    if strfind(probes(1).layout, 'Buzs') || strfind(probes(1).layout, 'Busz')
-      type{ii} = 'buzsaki';
-    else
-      type{ii} = 'linear';
-    end
-  else
-    error('unknown probe layout -- talk to ben');
-  end
-end
-
-probeFile = constructDataPath([exptdir filesep gridName '.probe'], grid, expt, 1);
-makeadjacencygraph2(layout, type, probeFile);
-
-nSitesPerShank = [];
-for ii = 1:length(layout)
-  nSitesPerShank = [nSitesPerShank repmat(layout{ii}(2), [1 layout{ii}(1)])];
-end
-
-fprintf('done\n');
