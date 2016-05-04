6e85cafdb95fae75503bbf44fb5ec6739a9f5063
Ben Willmore <ben@willmore.eu>
2016-05-04 13:53:15 +0100

    Add audioread compatibility for older matlab

---
+++ benware/sounds/audioread.m
@@ -0,0 +1,12 @@
+function [snd, fs] = audioread(filename)
+%% For backward compatibility:
+% If audioread exists, use it.
+% Otherwise use wavread.
+
+p = path;
+path(pathdef);
+if ~isempty(which('audioread'))
+  [snd, fs] = audioread(filename);
+else
+  [snd, fs] = wavread(filename);
+end
